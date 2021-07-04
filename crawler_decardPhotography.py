5b780b30251de5be806ddb8d5482f06d352c21c3
saeki1120cat <83437191+saeki1120cat@users.noreply.github.com>
2021-07-04 16:08:45 +0800

    Update crawler_decardPhotography.py

--- crawler_decardPhotography.py
+++ crawler_decardPhotography.py
@@ -1,5 +1,5 @@
+import random,time
 from selenium.webdriver import Chrome
-import time
 
 url = 'https://www.dcard.tw/f'
 
@@ -9,19 +9,14 @@ driver.get(url)
 
 driver.find_element_by_tag_name('input').send_keys('攝影')
 driver.find_element_by_xpath('//*[@id="__next"]/div[1]/div/div[1]/div/div/form/button[2]').click()
-
-time.sleep(3)
+time.sleep(random.randint(2,6))
 
 driver.execute_script('var s = document.documentElement.scrollTop=5000')
-
-time.sleep(3)
+time.sleep(random.randint(2,6))
 
 driver.execute_script('var s = document.documentElement.scrollTop=0')
-
-time.sleep(3)
+time.sleep(random.randint(2,6))
 
 driver.execute_script('var s = document.documentElement.scrollTop=5000')
-
 html = driver.execute_script("return document.getElementsByTagName('html')[0].outerHTML")
-
 print(html)
